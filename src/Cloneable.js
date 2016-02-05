28c5e97ae88c845791ca8f4347359914e0e41138
Lars Reimann <mail@lars-reimann.eu>
2016-02-05 12:34:02 +0100

    Improved the implementation of the Cloneable mixin

--- src/Cloneable.js
+++ src/Cloneable.js
@@ -4,8 +4,22 @@
  * @interface
  */
 export default class Cloneable {
-    clone() {}
+
+    /**
+     * Returns a copy of this object.
+     *
+     * @return {*}
+     * A copy of this object.
+     */
+    [cloneSym]() {}
 }
+
+/**
+ * A symbol for the clone method in the Cloneable interface.
+ *
+ * @type {Symbol}
+ */
+export const cloneSym = Symbol("clone");
 
 /**
  * This is a mixin for extensible behavior.
@@ -14,7 +28,76 @@ export default class Cloneable {
  * @todo
  */
 export const cloneableMixin = {
-    clone() {
-        return this; // TODO maybe use a more sophisticated method that goes through all properties of an object
+    [cloneSym]() {
+        return clone(this);
     }
 };
+
+function clone(p) {
+    if (isPrimitive(p)) {
+        return clonePrimitive(p);
+    }
+    if (isDate(p)) {
+        return cloneDate(p);
+    }
+    if (isArray(p)) {
+        return cloneArray(p);
+    }
+    if (isObject(p)) {
+        return cloneObject(p);
+    }
+
+    throw new Error(`${p} cannot be cloned.`);
+}
+
+function isPrimitive(p) {
+    return p === null || typeof p !== "object";
+}
+
+function clonePrimitive(primitive) {
+    return primitive;
+}
+
+function isDate(p) {
+    return p instanceof Date;
+}
+
+function cloneDate(date) {
+    let result = new Date();
+    result.setTime(date.getTime());
+    return result;
+}
+
+function isArray(p) {
+    return Array.isArray(p);
+}
+
+function cloneArray(arr) {
+    let result = [];
+    for (let v of arr) {
+        const copy = isCloneable(v) ? cloneCloneable(v) : clone(v);
+        result.push(copy);
+    }
+    return result;
+}
+
+function isObject(p) {
+    return p instanceof Object;
+}
+
+function cloneObject(obj) {
+    let result = {};
+    for (var [k, v] of obj.entries()) {
+        const copy = isCloneable(v) ? cloneCloneable(v) : clone(v);
+        result[k] = copy;
+    }
+    return result;
+}
+
+function isCloneable(p) {
+    return typeof p[cloneSym] === "function";
+}
+
+function cloneCloneable(cloneable) {
+    return cloneable[cloneSym]();
+}
