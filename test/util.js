6dacd2c094e5f53372c8cc367294f220b937d8ad
Lars Reimann <mail@lars-reimann.eu>
2016-02-06 17:56:55 +0100

    Added tests for the Cloneable mixin

--- test/util.js
+++ test/util.js
@@ -1,11 +1,13 @@
 import mocha       from "mocha";
 
+import cloneable   from "./cloneable.js";
 import cloner      from "./cloner.js";
 import color       from "./color.js";
 import idGenerator from "./idGenerator.js";
 
 mocha.setup("bdd");
 
+cloneable();
 cloner();
 color();
 idGenerator();
