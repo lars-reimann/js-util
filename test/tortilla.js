a9f09c329587a552a2ee3acb0567d0181d3329dd
Lars Reimann <mail@lars-reimann.eu>
2016-03-19 10:11:25 +0100

    Updated tests

--- test/tortilla.js
+++ test/tortilla.js
@@ -2,7 +2,7 @@ import {expect} from "chai";
 
 import {tortilla} from "../src/util.js";
 
-describe("tortilla", function () {
+describe("TortillaWrapper", function () {
     beforeEach(function () {
         this.inf = tortilla(function* (first = 0) {
             for (let n = first; ; n++) {
@@ -73,16 +73,21 @@ describe("tortilla", function () {
         });
 
         it("should drop the first value (finite)", function () {
-            const r0 = [...this.fin.tail()];
-            expect(r0).to.eql(["B", "C"]);
+            const r0 = [...this.fin.tail()].join("");
+            expect(r0).to.equal("BC");
         });
 
-        it("should allow multiple invocations", function () {
-            const r0 = this.inf.tail().head();
-            expect(r0).to.equal(1);
+        it("should drop the first value (iterable)", function () {
+            const r0 = [...this.iterable.tail()].join("");
+            expect(r0).to.equal("ello");
+        });
 
-            const r1 = this.inf.tail().head();
-            expect(r1).to.equal(1);
+        it("should drop the first value (iterator)", function () {
+            const r0 = [...this.iterator.tail()].join("");
+            expect(r0).to.equal("orld");
+
+            const r1 = [...this.iterator.tail()].join("");
+            expect(r1).to.equal("orld");
         });
     });
 
@@ -97,12 +102,17 @@ describe("tortilla", function () {
             expect(r0).to.eql([["A", "B"], ["C"]]);
         });
 
-        it("should allow multiple invocations", function () {
-            const r0 = this.inf.chunk(2).head();
-            expect(r0).to.eql([0, 1]);
+        it("should yield chunks of the given size (iterable)", function () {
+            const r0 = [...this.iterable.chunk(2)];
+            expect(r0).to.eql([["H", "e"], ["l", "l"], ["o"]]);
+        });
 
-            const r1 = this.inf.chunk(2).head();
-            expect(r1).to.eql([0, 1]);
+        it("should yield chunks of the given size (iterator)", function () {
+            const r0 = [...this.iterator.chunk(2)];
+            expect(r0).to.eql([["w", "o"], ["r", "l"], ["d"]]);
+
+            const r1 = [...this.iterator.chunk(2)];
+            expect(r1).to.eql([["w", "o"], ["r", "l"], ["d"]]);
         });
     });
 
@@ -113,16 +123,21 @@ describe("tortilla", function () {
         });
 
         it("should remove all falsy values (finite)", function () {
-            const r0 = [...this.fin.compact()];
-            expect(r0).to.eql(["A", "B", "C"]);
+            const r0 = [...this.fin.compact()].join("");
+            expect(r0).to.equal("ABC");
         });
 
-        it("should allow multiple invocations", function () {
-            const r0 = this.inf.compact().head();
-            expect(r0).to.equal(1);
+        it("should remove all falsy values (iterable)", function () {
+            const r0 = [...this.iterable.compact()].join("");
+            expect(r0).to.equal("Hello");
+        });
 
-            const r1 = this.inf.compact().head();
-            expect(r1).to.equal(1);
+        it("should remove all falsy values (iterator)", function () {
+            const r0 = [...this.iterator.compact()].join("");
+            expect(r0).to.equal("world");
+
+            const r1 = [...this.iterator.compact()].join("");
+            expect(r1).to.equal("world");
         });
     });
 
@@ -133,16 +148,21 @@ describe("tortilla", function () {
         });
 
         it("should remove all matching values (finite)", function () {
-            const r0 = [...this.fin.without(["B"])];
-            expect(r0).to.eql(["A", "C"]);
+            const r0 = [...this.fin.without(["B"])].join("");
+            expect(r0).to.equal("AC");
         });
 
-        it("should allow multiple invocations", function () {
-            const r0 = this.inf.without([0, 1]).head();
-            expect(r0).to.equal(2);
+        it("should remove all matching values (iterable)", function () {
+            const r0 = [...this.iterable.without(["H"])].join("");
+            expect(r0).to.equal("ello");
+        });
 
-            const r1 = this.inf.without([0, 1]).head();
-            expect(r1).to.equal(2);
+        it("should remove all matching values (iterator)", function () {
+            const r0 = [...this.iterator.without(["w"])].join("");
+            expect(r0).to.equal("orld");
+
+            const r1 = [...this.iterator.without(["w"])].join("");
+            expect(r1).to.equal("orld");
         });
     });
 
@@ -153,16 +173,21 @@ describe("tortilla", function () {
         });
 
         it("should only yield values in the given range (finite)", function () {
-            const r0 = [...this.fin.slice(1, 2)];
-            expect(r0).to.eql(["B"]);
+            const r0 = [...this.fin.slice(1, 2)].join("");
+            expect(r0).to.equal("B");
         });
 
-        it("should allow multiple invocations", function () {
-            const r0 = [...this.inf.slice(1, 3)];
-            expect(r0).to.eql([1, 2]);
+        it("should only yield values in the given range (iterable)", function () {
+            const r0 = [...this.iterable.slice(1, 2)].join("");
+            expect(r0).to.equal("e");
+        });
 
-            const r1 = [...this.inf.slice(1, 3)];
-            expect(r1).to.eql([1, 2]);
+        it("should only yield values in the given range (iterator)", function () {
+            const r0 = [...this.iterator.slice(1, 2)].join("");
+            expect(r0).to.equal("o");
+
+            const r1 = [...this.iterator.slice(1, 2)].join("");
+            expect(r1).to.equal("o");
         });
     });
 
@@ -173,16 +198,21 @@ describe("tortilla", function () {
         });
 
         it("should drop the first n values (finite)", function () {
-            const r0 = [...this.fin.drop(4)];
+            const r0 = [...this.fin.drop(4)].join("");
             expect(r0).to.be.empty;
         });
 
-        it("should allow multiple invocations", function () {
-            const r0 = this.inf.drop(2).head();
-            expect(r0).to.equal(2);
+        it("should drop the first n values (iterable)", function () {
+            const r0 = [...this.iterable.drop(4)].join("");
+            expect(r0).to.equal("o");
+        });
 
-            const r1 = this.inf.drop(2).head();
-            expect(r1).to.equal(2);
+        it("should drop the first n values (iterator)", function () {
+            const r0 = [...this.iterator.drop(4)].join("");
+            expect(r0).to.equal("d");
+
+            const r1 = [...this.iterator.drop(4)].join("");
+            expect(r1).to.equal("d");
         });
     });
 
@@ -193,16 +223,21 @@ describe("tortilla", function () {
         });
 
         it("should drop all values until the predicate is true (finite)", function () {
-            const r0 = [...this.fin.dropWhile(x => x !== "D")];
+            const r0 = [...this.fin.dropWhile(x => x !== "D")].join("");
             expect(r0).to.be.empty;
         });
 
-        it("should allow multiple invocations", function () {
-            const r0 = this.inf.dropWhile(x => x < 2).head();
-            expect(r0).to.equal(2);
+        it("should drop all values until the predicate is true (iterable)", function () {
+            const r0 = [...this.iterable.dropWhile(x => x !== "e")].join("");
+            expect(r0).to.equal("ello");
+        });
 
-            const r1 = this.inf.dropWhile(x => x < 2).head();
-            expect(r1).to.equal(2);
+        it("should drop all values until the predicate is true (iterator)", function () {
+            const r0 = [...this.iterator.dropWhile(x => x !== "d")].join("");
+            expect(r0).to.equal("d");
+
+            const r1 = [...this.iterator.dropWhile(x => x !== "d")].join("");
+            expect(r1).to.equal("d");
         });
     });
 
@@ -213,16 +248,21 @@ describe("tortilla", function () {
         });
 
         it("should yield only the first n values (finite)", function () {
-            const r0 = [...this.fin.take(5)];
-            expect(r0).to.eql(["A", "B", "C"]);
+            const r0 = [...this.fin.take(5)].join("");
+            expect(r0).to.equal("ABC");
         });
 
-        it("should allow multiple invocations", function () {
-            const r0 = [...this.inf.take(5)];
-            expect(r0).to.eql([0, 1, 2, 3, 4]);
+        it("should yield only the first n values (iterable)", function () {
+            const r0 = [...this.iterable.take(4)].join("");
+            expect(r0).to.equal("Hell");
+        });
 
-            const r1 = [...this.inf.take(5)];
-            expect(r1).to.eql([0, 1, 2, 3, 4]);
+        it("should yield only the first n values (iterator)", function () {
+            const r0 = [...this.iterator.take(5)].join("");
+            expect(r0).to.equal("world");
+
+            const r1 = [...this.iterator.take(5)].join("");
+            expect(r1).to.equal("world");
         });
     });
 
@@ -233,16 +273,21 @@ describe("tortilla", function () {
         });
 
         it("should yield only the first value until the predicate is false (finite)", function () {
-            const r0 = [...this.fin.takeWhile(x => x !== "D")];
-            expect(r0).to.eql(["A", "B", "C"]);
+            const r0 = [...this.fin.takeWhile(x => x !== "D")].join("");
+            expect(r0).to.equal("ABC");
         });
 
-        it("should allow multiple invocations", function () {
-            const r0 = [...this.inf.takeWhile(x => x < 5)];
-            expect(r0).to.eql([0, 1, 2, 3, 4]);
+        it("should yield only the first value until the predicate is false (iterable)", function () {
+            const r0 = [...this.iterable.takeWhile(x => x !== "D")].join("");
+            expect(r0).to.equal("Hello");
+        });
 
-            const r1 = [...this.inf.takeWhile(x => x < 5)];
-            expect(r1).to.eql([0, 1, 2, 3, 4]);
+        it("should yield only the first value until the predicate is false (iterator)", function () {
+            const r0 = [...this.iterator.takeWhile(x => x !== "w")].join("");
+            expect(r0).to.be.empty;
+
+            const r1 = [...this.iterator.takeWhile(x => x !== "w")].join("");
+            expect(r1).to.be.empty;
         });
     });
 
@@ -253,16 +298,21 @@ describe("tortilla", function () {
         });
 
         it("should yield only matching values (finite)", function () {
-            const r0 = [...this.fin.filter(x => x !== "B")];
-            expect(r0).to.eql(["A", "C"]);
+            const r0 = [...this.fin.filter(x => x !== "B")].join("");
+            expect(r0).to.equal("AC");
         });
 
-        it("should allow multiple invocations", function () {
-            const r0 = this.inf.filter(x => x > 3).head();
-            expect(r0).to.equal(4);
+        it("should yield only matching values (iterable)", function () {
+            const r0 = [...this.iterable.filter(x => x !== "o")].join("");
+            expect(r0).to.equal("Hell");
+        });
 
-            const r1 = this.inf.filter(x => x > 3).head();
-            expect(r1).to.equal(4);
+        it("should yield only matching values (iterator)", function () {
+            const r0 = [...this.iterator.filter(x => x === "d")].join("");
+            expect(r0).to.equal("d");
+
+            const r1 = [...this.iterator.filter(x => x === "d")].join("");
+            expect(r1).to.equal("d");
         });
     });
 
@@ -273,16 +323,21 @@ describe("tortilla", function () {
         });
 
         it("should yield only matching values (finite)", function () {
-            const r0 = [...this.fin.reject(x => x === "B")];
-            expect(r0).to.eql(["A", "C"]);
+            const r0 = [...this.fin.reject(x => x === "B")].join("");
+            expect(r0).to.equal("AC");
         });
 
-        it("should allow multiple invocations", function () {
-            const r0 = this.inf.reject(x => x <= 3).head();
-            expect(r0).to.equal(4);
+        it("should yield only matching values (iterable)", function () {
+            const r0 = [...this.iterable.reject(x => x === "H")].join("");
+            expect(r0).to.equal("ello");
+        });
 
-            const r1 = this.inf.reject(x => x <= 3).head();
-            expect(r1).to.equal(4);
+        it("should yield only matching values (iterator)", function () {
+            const r0 = [...this.iterator.reject(x => x === "d")].join("");
+            expect(r0).to.equal("worl");
+
+            const r1 = [...this.iterator.reject(x => x === "d")].join("");
+            expect(r1).to.equal("worl");
         });
     });
 
@@ -293,41 +348,51 @@ describe("tortilla", function () {
         });
 
         it("should apply the function on each value and yield the results (finite)", function () {
-            const r0 = [...this.fin.map(x => x.toLowerCase())];
-            expect(r0).to.eql(["a", "b", "c"]);
+            const r0 = [...this.fin.map(x => x.toLowerCase())].join("");
+            expect(r0).to.equal("abc");
         });
 
-        it("should allow multiple invocations", function () {
-            const r0 = this.inf.map(x => x * 3).head();
-            expect(r0).to.equal(0);
+        it("should apply the function on each value and yield the results (iterable)", function () {
+            const r0 = [...this.iterable.map(x => x.toLowerCase())].join("");
+            expect(r0).to.equal("hello");
+        });
 
-            const r1 = this.inf.map(x => x * 3).head();
-            expect(r1).to.equal(0);
+        it("should apply the function on each value and yield the results (iterator)", function () {
+            const r0 = [...this.iterator.map(x => x.toLowerCase())].join("");
+            expect(r0).to.equal("world");
+
+            const r1 = [...this.iterator.map(x => x.toLowerCase())].join("");
+            expect(r1).to.equal("world");
         });
     });
 
     describe("flatten", function () {
-        it("should yield the values of array separately (infinite)", function () {
+        it("should yield the values of arrays separately (infinite)", function () {
             const r0 = this.inf.chunk(3).flatten().head();
             expect(r0).to.equal(0);
         });
 
-        it("should yield the values of array separately (finite)", function () {
-            const r0 = [...this.fin.chunk(3).flatten()];
-            expect(r0).to.eql(["A", "B", "C"]);
+        it("should yield the values of arrays separately (finite)", function () {
+            const r0 = [...this.fin.chunk(3).flatten()].join("");
+            expect(r0).to.equal("ABC");
         });
 
-        it("should allow multiple invocations", function () {
-            const r0 = this.inf.chunk(3).flatten().head();
-            expect(r0).to.equal(0);
+        it("should yield the values of arrays separately (iterable)", function () {
+            const r0 = [...this.iterable.chunk(3).flatten()].join("");
+            expect(r0).to.equal("Hello");
+        });
 
-            const r1 = this.inf.chunk(3).flatten().head();
-            expect(r1).to.equal(0);
+        it("should yield the values of arrays separately (iterator)", function () {
+            const r0 = [...this.iterator.chunk(3).flatten()].join("");
+            expect(r0).to.equal("world");
+
+            const r1 = [...this.iterator.chunk(3).flatten()].join("");
+            expect(r1).to.equal("world");
         });
     });
 
     describe("apply", function () {
-        it("should partially apply the given parameters", function () {
+        it("should partially apply the given parameters (generator function)", function () {
             const r0 = this.inf.apply(4).head();
             expect(r0).to.equal(4);
 
